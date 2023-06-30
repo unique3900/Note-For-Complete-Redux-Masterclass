5f575d45c3b1c795bf4a93eef621966839ba941c
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-06-30 21:00:05 +0545

    concept of payload and action creators

--- Day01/Redux.js
+++ Day01/Redux.js
@@ -24,6 +24,18 @@ function reducer(state={amount:1}, action){
         return {amount:state.amount+1}
     }
 
+    if (action.type == 'decreament') {
+        return {
+            amount: state.amount - 1
+        }
+    }
+
+    if (action.type == 'increaseByAmount') {
+        return {
+            amount: state.amount + action.payload
+        }
+    }
+
 
     //Reducer always returns the state
     // We can return new state after calculation
@@ -48,8 +60,16 @@ store.subscribe(() => {
 // Action is a convention
 // Not defined by Default only a convention=
 
-setInterval(()=>{
-    store.dispatch({ type: 'increament' });
+
+// Day 01: Topic 3 Action Creators
+
+const funcInc = () => {
+    return {type:'increament'}
+}
+setInterval(() => {
+    // store.dispatch({type:'decreament'})
+    // store.dispatch({ type: 'increaseByAmount',payload:10 });
+    store.dispatch(funcInc())
 },5000)
 
 
