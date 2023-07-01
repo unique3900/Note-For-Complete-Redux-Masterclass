caae5154b85d9a9a2966f680850e038d2b77a45b
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-07-01 15:41:17 +0545

    Multiple Reducer with combineReducer

--- Day02/index.js
+++ Day02/index.js
@@ -1,9 +1,10 @@
 // Day 2 Topic 1: Multiple Reducers
+// CombineReducers
 
 
 import axios from "axios";
-import { applyMiddleware, createStore } from "redux";
-import  logger from "redux-logger";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import logger from 'redux-logger';
 import thunk from "redux-thunk";
 
 const history = [];
@@ -12,9 +13,13 @@ const history = [];
 // Action Type
 const init = 'initUser';
 const inc = 'increament';
+const incByAmt='increaseByValue'
 const dec = 'decreament';
 
-const store=createStore(accountReducer,applyMiddleware(logger.default,thunk.default))
+const store = createStore(combineReducers({
+    account: accountReducer,
+    bonus:bonusReducer
+}),applyMiddleware(logger.default,thunk.default))
 
 // Reducer 1
 function accountReducer(state = { amount: 1 }, action) {
@@ -25,15 +30,36 @@ function accountReducer(state = { amount: 1 }, action) {
             return { amount: state.amount + 1 }
         case dec:
             return { amount: state.amount - 1 }
+        case incByAmt:
+            return {amount:state.amount + action.payload}
         default:
             return state;
     }
 }
 // Reducer 2
-function bonusReducer(state = { points: 0 }, account) {
+function bonusReducer(state = { points: 0 }, action) {
     switch (action.type) {
-        case inc: {
-            return {points:state.bonus + 1 }
-        }
+        case incByAmt: 
+            if(action.payload>100)
+                    return {points:state.points + 3 }
+        default:
+            return state;
     }
 }
+
+
+// Action creator
+ function getUser(id) {
+    return async (dispatch, getState)=>{
+        const { data } = await axios.get(`http://localhost:3000/account/${id}`);
+        dispatch(increase(data.amount));
+    }
+}
+
+function increase(value) {
+    return {type:incByAmt,payload:value}
+}
+
+setTimeout(() => {
+    store.dispatch(increase(200))
+}, 2000);
