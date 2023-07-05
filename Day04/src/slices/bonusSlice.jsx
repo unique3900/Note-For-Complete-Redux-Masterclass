b043079d16259ba4547f044d25d98069c8a013d3
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-07-05 12:02:13 +0545

    concept of extra reducers in redux toolkit

--- Day04/src/slices/bonusSlice.jsx
+++ Day04/src/slices/bonusSlice.jsx
@@ -1,9 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
 import { increament } from "./accountSlice";
 
 const initialState = {
     points:2
 }
+//Yedi accountslice ko amount badhyo vane bonus pani badhne case ma
+// We need to create action first
+
+const incAmount=createAction('account/incByAmt') //yo redux dev tool ko inspctor bata tanna milxa
 export const bonusSlice = createSlice({
     name: 'bonus',
     initialState,
@@ -11,6 +15,17 @@ export const bonusSlice = createSlice({
         increamentBonus: (state) => {
             state.points +=1
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(incAmount, (state, action) => {
+            if (action.payload > 5) {
+                state.points += 2;
+            }
+            else {
+                state.points += 1;
+            }
+            
+        })
     }
 })
 
