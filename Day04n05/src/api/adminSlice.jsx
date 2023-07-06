b4c58a2fa54ebd68bbb075cd6876ba2bf095eba0
uniqueneu3900 <62101934+unique3900@users.noreply.github.com>
2023-07-06 13:43:28 +0545

    REST API with RTK query complete

--- Day04n05/src/api/adminSlice.jsx
+++ Day04n05/src/api/adminSlice.jsx
@@ -7,7 +7,38 @@ export const adminApi = createApi({
     endpoints: (builder) => ({
         getAccounts: builder.query({
             // Api ko path kun lai query garne?
-            query:()=>`account`
+            query: () => `account`,
+
+            //Important to add providesTag for realtime update
+            providesTags:['account']
+        }),
+        // Only get is done using query other POST,put... is done by mutation
+        addAccounts: builder.mutation({
+            query: (amount,id) => ({
+                url: 'account',
+                method: 'POST',
+                body:{amount,id}
+            }),
+            //Caching hataidincha
+            invalidatesTags: ['account']
+            
+        }),
+        deleteAccount: builder.mutation({
+            query: (id) => ({
+                url: `account/${id}`,
+                method: 'DELETE',
+                body:{id}
+            }),
+            invalidatesTags:['account']
+        }),
+        updateAccount: builder.mutation({
+            query: ({amount, id}) => ({
+                url: `account/${id}`,
+                method: 'PATCH',
+                body:{amount,id}
+
+            }),
+            invalidatesTags:['account']
         })
     })
 })
@@ -15,4 +46,4 @@ export const adminApi = createApi({
 // Export Hook
 // Custom hook,aagadi use Lekhne auto option aaucha
 
-export const {useGetAccountsQuery } = adminApi;
+export const {useGetAccountsQuery,useAddAccountsMutation ,useDeleteAccountMutation,useUpdateAccountMutation} = adminApi;
--- Day04n05/src/components/Admin.jsx
+++ Day04n05/src/components/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useGetAccountsQuery } from '../api/adminSlice'
+import { useAddAccountsMutation, useDeleteAccountMutation, useGetAccountsQuery, useUpdateAccountMutation } from '../api/adminSlice'
 
 // Day 05 RTK qurty (Optional of asyncThunk)
 const Admin = () => {
@@ -7,6 +7,14 @@ const Admin = () => {
     // Destructure data,error,isLoading is prebuild
     const { data, error, isLoading } = useGetAccountsQuery();
 
+    // Now for POST method
+    const [addAccount, response] = useAddAccountsMutation(); //addAccount ko thau ma j nam deye pani huncha tala dispatcher jasari kam garxa
+
+    // Delete Method
+    const [deleteAccount, resp] = useDeleteAccountMutation();
+
+    // Update
+    const [updateAccount] = useUpdateAccountMutation();
   return (
     <div className='b-2 p-5 mt-10'>
           <h1 className="text-red">Admin Component</h1>
@@ -15,10 +23,25 @@ const Admin = () => {
               <h4 className="text-yellow p-5">Admin Component </h4>
               {
                   data && data.map((item, index) => (
-                      <p className="text-white" key={index}>{index} :
-                          { item.amount }</p>
+                      <div className='flex-row'>
+                          <p className="text-white" key={index}>{index} :
+                              {item.amount}</p>
+                          <button className="" onClick={() => {
+                              deleteAccount(item.id)
+                          }} >Delete</button>
+                         <button className="" onClick={() => {
+                              updateAccount({amount:6969,id:item.id})
+                          }} >Update</button>
+                          
+                      </div>
+                      
+                      
+                      
                   ))
-       }
+              }
+              <button onClick={() => {
+                  addAccount(2999,data.length-1)
+              }}>Add Account</button>
              
           </div>
   </div>
